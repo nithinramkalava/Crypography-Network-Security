d7f1ce42e5b9e57ee49aa04c145059dec40339d2
nithinramkalava <nithinramkalava@gmail.com>
2024-03-21 09:48:57 +0530

    MAC update

--- MAC.java
+++ MAC.java
@@ -1,53 +1,10 @@
-import java.security.Key;
-import java.security.SecureRandom;
-import java.util.Base64;
-import java.util.Scanner;
-
-import javax.crypto.KeyGenerator;
-import javax.crypto.Mac;
-
-public class MessageAuthenticationCodes {
-   public static void main(String args[]) throws Exception{
-      //Creating a KeyGenerator object
-      KeyGenerator keyGen = KeyGenerator.getInstance("DES");
-
-      //Creating a SecureRandom object
-      SecureRandom secRandom = new SecureRandom();
-
-      //Initializing the KeyGenerator
-      keyGen.init(secRandom);
-
-      //Creating/Generating a key
-      Key key = keyGen.generateKey();     
-
-      //Creating a Mac object
-      Mac mac = Mac.getInstance("HmacSHA256");
-
-      //Initializing the Mac object
-      mac.init(key);
-
-      //Computing the Mac
-      Scanner scanner = new Scanner(System.in);
-      System.out.println("Enter Message: ");
-      String msg = scanner.nextLine();
-      scanner.close();
-      byte[] bytes = msg.getBytes();      
-      byte[] macResult = mac.doFinal(bytes);
-
-      System.out.println("Mac result:");
-      System.out.println(Base64.getEncoder().encodeToString(macResult));     
-   }
-}
-
-
-
 import javax.crypto.*;
 import javax.crypto.spec.SecretKeySpec;
 import java.nio.charset.StandardCharsets;
 import java.security.*;
 import java.util.Scanner;
 
-public class MessageAuthenticationCodes {
+public class MAC {
 
     public static void main(String[] args) throws NoSuchAlgorithmException, InvalidKeyException {
         // Message to be authenticated
